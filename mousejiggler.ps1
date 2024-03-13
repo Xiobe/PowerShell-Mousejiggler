0252c34ea2a83ed37aa8ed3b35b43c8946747145
Xiobe <3995581+Xiobe@users.noreply.github.com>
2024-03-13 21:42:28 +0100

    Add files via upload

---
+++ mousejiggler.ps1
@@ -0,0 +1,15 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+while ($true):
+{
+  # Get the position of the cursor
+  $pos = [System.Windows.Forms.Cursor]::Position
+  # Calculate a new position
+  $x = ($pos.X) + 1
+  $y = ($pos.Y) + 1
+  # Move the cursor to the new position and back to the original one
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($pos.X,$pos.Y)
+  # Wait
+  Start-Sleep -Seconds 10
+}
