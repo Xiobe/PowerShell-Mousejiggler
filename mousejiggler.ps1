f253b7171cd1560c5f1df92cb3d003ec90e8f7a4
Xiobe <3995581+Xiobe@users.noreply.github.com>
2024-06-25 10:55:47 +0200

    Update mousejiggler.ps1

--- mousejiggler.ps1
+++ mousejiggler.ps1
@@ -1,15 +1,32 @@
+<#
+.SYNOPSIS
+The idea for this script was to create a mouse jiggler to keep a remote rdp session alive.
+
+.DESCRIPTION
+Because the session needed to be kept alive, we used a while true loop.
+
+The basic logic is simple. We first need to establish the height and the width of the screen
+and then can randomly calculate a new x and y for the cursor. The wait has been randomized
+for fun.
+
+.LINK
+https://github.com/Xiobe/PowerShell-Mousejiggler
+
+.Example
+ jiggle
+#>
 Add-Type -AssemblyName System.Windows.Forms
+$screen = [system.windows.forms.systeminformation]::virtualscreen
 
 while ($true)
 {
-  # Get the position of the cursor
-  $pos = [System.Windows.Forms.Cursor]::Position
-  # Calculate a new position
-  $x = ($pos.X) + 1
-  $y = ($pos.Y) + 1
-  # Move the cursor to the new position and back to the original one
-  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
-  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($pos.X,$pos.Y)
+  # Get random integers for the height and width of the screen
+  $x = Get-Random -Maximum $screen.width -Minimum 0
+  $y = Get-Random -Maximum $screen.height -Minimum 0
+  $wait = Get-Random -Maximum 10 -Minimum 0
+
+  # Move the cursor to the new position
+  [windows.forms.cursor]::Position = "$($x),$($y)"
   # Wait
-  Start-Sleep -Seconds 10
+  Start-Sleep -Seconds $waitYe
 }
